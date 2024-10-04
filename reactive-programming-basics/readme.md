a589d4daab4305dd4190557a065690a2d7810c88
himanshubhusan.rath <himanshubhusan.rath@landmarkgroup.com>
2024-10-04 20:07:49 +0530

    Initial commit

---
+++ reactive-programming-basics/readme.md
@@ -0,0 +1,2 @@
+## Reactive Programming
+* Check the classes under `com.hr.reactiveprogramming.examples` package to see examples of Reactive programming using`Mono` and `Flux`
---
+++ reactive-programming-basics/src/main/java/com/hr/reactiveprogramming/ReactiveProgrammingBasicsMain.java
@@ -0,0 +1,12 @@
+package com.hr.reactiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingBasicsMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgrammingBasicsMain.class, args);
+    }
+
+}
